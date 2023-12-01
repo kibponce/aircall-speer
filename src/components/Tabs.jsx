3bd4af1cbf824bdad14d20d2ad72486d2eb8d234
Kevin Ponce <kib.ponce@gmail.com>
2023-12-01 03:17:52 -0500

    remove filter by element type
    production issue were type name is null

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 const Tabs = ({ value, onChange, children }) => {
-  // display "Tab" elements only
-  const tabElements = children.filter((child) => child.type.name === "Tab");
-  console.log("children", children);
   return (
     <div className="tabs">
       <ul className="tabs-nav">
-        {tabElements.map((child, index) => {
+        {children.map((child, index) => {
           // clone the child elements so we can send custom property values
           return React.cloneElement(child, {
             key: index,
